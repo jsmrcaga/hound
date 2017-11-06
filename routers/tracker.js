7daa2101bc3940b4050ef21098056f52cdbd8116
Jo Colina <jo@jocolina.com>
2017-11-06 10:20:45 +0100

    Added ignore capabilities for shorteners (todo: add for social networks previews)

--- routers/tracker.js
+++ routers/tracker.js
@@ -1,5 +1,6 @@
 const join = require('path').join;
 const Tracking = require(join(__dirname, '../tracking/tracking'));
+const Utils = require(join(__dirname,  '../lib/utils'));
 let tracker = require('express').Router();
 
 tracker.post('/track', function(req, res, next){
@@ -10,6 +11,12 @@ tracker.get('/pixel/:id', function(req, res, next){
 	let pixel = DB.Pixels.get(req.params.id);
 	if(!pixel){
 		return res.status(404).json({error:{message:`Pixel ${req.params.id} not found`, code:404}});
+	}
+
+	if(Utils.ignore(req)){
+		return res.sendFile('./pixel.gif', {
+				root: join(__dirname + '../../../files')
+		});
 	}
 
 	Tracking.pixel(pixel, req);
@@ -26,6 +33,10 @@ tracker.get('/:campaign', function(req, res, next){
 	} else if(!campaign && !req.query.redirect_uri){
 		console.error(`Campaign ${req.params.campaign} not found and no fallback URI given`);
 		return res.redirect('https://google.com');
+	}
+
+	if(Utils.ignore(req)){
+		return res.redirect(campaign.redir || req.query.redirect_uri || 'https://google.com');
 	}
 
 	let url = Tracking.track(campaign, req);
